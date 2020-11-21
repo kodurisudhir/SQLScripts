e268a345c79038ad3102d40ff4b42a42c6301dbc
Koduri Sudhir <koduri.sudhir@gmail.com>
2020-11-21 01:14:12 -0600

    fix issues in the script

--- ListAllStepsInSSISPackage.sql
+++ ListAllStepsInSSISPackage.sql
@@ -6,17 +6,15 @@ Set @ProjectName=''
 Set @PackageName=''
 
 ;With CTE as (Select Max(package_id) over (partition by C.Project_id,C.[name]) as LatestPackageId,Package_id,
-                      A.[Name] as [FolderName],B.[Name] as [ProjectName],C.[Name] as [PackageName],
-					  D.executable_name as ExecutableName,D.Executable_id,Package_path as PackagePath
-
+                     A.[Name] as [FolderName],B.[Name] as [ProjectName],C.[Name] as [PackageName],
+					 D.executable_name as ExecutableName,D.Executable_id,Package_path as PackagePath
                 from [SSISDB].[Internal].[Folders] A INNER JOIN 
                  	 [SSISDB].[Internal].[Projects] B ON A.folder_id=B.folder_id INNER JOIN 
-	                 [SSISDB].[Internal].[Packages] C on C.project_id=B.project_id INNER JOIN 
-	                 [SSISDB].[Internal].[Executables] D on B.project_id=D.project_id and C.[Name]=D.Package_Name and B.object_version_lsn=D.project_version_lsn
-               
-			   where B.[Name]=@ProjectName and 
-			         C.[Name]=@PackageName and
-                     D.Package_path not like '\Package')
+	                 [SSISDB].[Internal].[Packages] C ON C.project_id=B.project_id INNER JOIN 
+	                 [SSISDB].[Internal].[Executables] D ON B.project_id=D.project_id and C.[Name]=D.Package_Name and B.object_version_lsn=D.project_version_lsn
+               where B.[Name]=@ProjectName
+			         and C.[Name]=@PackageName
+                     and D.Package_path not like '\Package')
 
 Select FolderName,ProjectName,PackageName,ExecutableName,PackagePath
   from CTE 
