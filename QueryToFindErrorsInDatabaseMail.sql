b47161cec22e9f1e41b186e9ce7aae67a526fc69
Koduri Sudhir <koduri.sudhir@gmail.com>
2020-11-26 10:16:20 -0600

    changes to database mail query

--- QueryToFindErrorsInDatabaseMail.sql
+++ QueryToFindErrorsInDatabaseMail.sql
@@ -1,14 +1,13 @@
 -- Query to find Errors in Database Mail.
 Declare @Logdate datetime  = DateAdd(day,-1,getdate())
 
-;WITH Cte as (Select A.MailItem_id,Max(log_date) as [LogDate]
-                from msdb.dbo.sysmail_faileditems A INNER JOIN
-                     msdb.dbo.sysmail_event_log B on A.Mailitem_id=B.mailitem_id
-			   where Log_date>@Logdate
-               group by A.MailItem_id)
+;WITH Cte as (Select MailItem_id,Max(log_date) as [LogDate]
+                from msdb.dbo.sysmail_event_log B
+	       where Log_date>@Logdate
+               group by MailItem_id)
 
 Select A.MailItem_id,B.[Description] as [Error],C.Subject as [Mail Subject],C.Recipients as [Mail Recipients],Logdate
   from Cte A INNER JOIN
-       msdb.dbo.sysmail_event_log B on A.MailItem_id=B.MailItem_ID and A.LogDate=B.Log_Date INNER JOIN
-	   msdb.dbo.sysmail_faileditems C on C.MailItem_id=B.MailItem_ID
+       msdb.dbo.sysmail_event_log B on A.MailItem_id=B.MailItem_id and A.LogDate=B.Log_Date INNER JOIN
+       msdb.dbo.sysmail_faileditems C on C.MailItem_id=B.MailItem_id
 
